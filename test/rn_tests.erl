0141621cac2a982d2a21f0f2925e1ce5b6a216e3
Gianfranco Alongi <zenon@zentop.(none)>
2013-06-19 21:03:05 +0200

    Refactored the tests into a list of Input, Expected pair tuples

--- test/rn_tests.erl
+++ test/rn_tests.erl
@@ -1,23 +1,18 @@
 -module(rn_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-one_to_roman_test() ->
-    ?assertEqual("I",rn:to_roman(1)).
+roman_numeral_test_() ->
+    lists:map(fun make_test/1,[
+			       {1,"I"},
+			       {2,"II"},
+			       {3,"III"},
+			       {4,"IV"},
+			       {5,"V"},
+			       {6,"VI"},
+			       {7,"VII"}
+			      ]).
 
-two_to_roman_test() ->
-    ?assertEqual("II",rn:to_roman(2)).
-
-three_to_roman_test() ->
-    ?assertEqual("III",rn:to_roman(3)).
-
-four_to_roman_test() ->
-    ?assertEqual("IV",rn:to_roman(4)).
-
-five_to_roman_test() ->
-    ?assertEqual("V",rn:to_roman(5)).
-
-six_to_roman_test() ->
-    ?assertEqual("VI",rn:to_roman(6)).
-
-seven_to_roman_test() ->
-    ?assertEqual("VII",rn:to_roman(7)).
+make_test({Input,Expected}) ->
+    fun() -> 
+	    ?assertEqual(Expected,rn:to_roman(Input))
+    end.
