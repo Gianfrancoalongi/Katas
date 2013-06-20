ca6363acbd7f53609e9fa8b7e8a1b5aa12a1799c
Gianfranco Alongi <zenon@zentop.(none)>
2013-06-21 00:45:27 +0200

    Refactored, removed a superfluous conditional test

--- src/rn.erl
+++ src/rn.erl
@@ -21,7 +21,7 @@ to_roman(X) when X < 40 ->
     to_roman(10)++to_roman(X-10);
 to_roman(X) when X == 40 ->
     "XL";
-to_roman(X) when X > 40 andalso X < 50 ->
+to_roman(X) when X < 50 ->
     to_roman(40)++to_roman(X-40);
 to_roman(X) when X == 50 ->
     "L";        
