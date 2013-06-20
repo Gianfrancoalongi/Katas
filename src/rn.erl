ae8414200b3781f5adbe0adead683a7aa9fab08d
Gianfranco Alongi <zenon@zentop.(none)>
2013-06-21 00:57:31 +0200

    Refactoring Removing guard statements where pattern matching can be utilized instead

--- src/rn.erl
+++ src/rn.erl
@@ -13,23 +13,23 @@ to_roman(X) when X < 9 ->
     to_roman(5)++to_roman(X-5);
 to_roman(X) when X < 10 ->
     "IX";
-to_roman(X) when X == 10 ->
+to_roman(10) ->
     "X";
 to_roman(X) when X < 40 ->
     to_roman(10)++to_roman(X-10);
-to_roman(X) when X == 40 ->
+to_roman(40) ->
     "XL";
 to_roman(X) when X < 50 ->
     to_roman(40)++to_roman(X-40);
-to_roman(X) when X == 50 ->
+to_roman(50) ->
     "L";        
 to_roman(X) when X < 90 ->
     to_roman(50)++to_roman(X-50);
-to_roman(X) when X == 90 ->
+to_roman(90) ->
     "XC";
 to_roman(X) when X < 100 ->
     to_roman(90)++to_roman(X-90);
-to_roman(X) when X == 100 ->
+to_roman(100) ->
     "C";
 to_roman(X) ->
     to_roman(100)++to_roman(X-100).
