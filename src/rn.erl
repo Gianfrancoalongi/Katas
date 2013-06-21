b8c73e34958b9201af399ef124f0b16568a43027
Gianfranco Alongi <zenon@zentop.(none)>
2013-06-21 13:17:02 +0200

    Refactored. First part of algorithmic implementation.

--- src/rn.erl
+++ src/rn.erl
@@ -1,41 +1,27 @@
 -module(rn).
 -export([to_roman/1]).
 
-to_roman(1) ->
-    "I";
-to_roman(X) when X < 4->
-    to_roman(1)++to_roman(X-1);
-to_roman(4) ->
-    "IV";
-to_roman(5) ->
-    "V";
-to_roman(X) when X < 9 ->
-    to_roman(5)++to_roman(X-5);
-to_roman(9) ->
-    "IX";
-to_roman(10) ->
-    "X";
-to_roman(X) when X < 40 ->
-    to_roman(10)++to_roman(X-10);
-to_roman(40) ->
-    "XL";
-to_roman(X) when X < 50 ->
-    to_roman(40)++to_roman(X-40);
-to_roman(50) ->
-    "L";        
-to_roman(X) when X < 90 ->
-    to_roman(50)++to_roman(X-50);
-to_roman(90) ->
-    "XC";
-to_roman(X) when X < 100 ->
-    to_roman(90)++to_roman(X-90);
-to_roman(100) ->
-    "C";
-to_roman(X) when X < 400 ->
-    to_roman(100)++to_roman(X-100);
-to_roman(400) ->
-    "CD";
+to_roman(X) when X < 1 -> "";
 to_roman(X) ->
-    to_roman(400)++to_roman(X-400).
+    Slices = [{{1,3},"I"},
+	      {{4,4},"IV"},
+	      {{5,8},"V"},
+	      {{9,9},"IX"},
+	      {{10,39},"X"},
+	      {{40,49},"XL"},
+	      {{50,89},"L"},
+	      {{90,99},"XC"},
+	      {{100,399},"C"},
+	      {{400,499},"CD"}
+	     ],
+    {Xp,Part} = select_matching_slice_and_remove_lower_bound(X,Slices),
+    Part++to_roman(Xp).
 
+select_matching_slice_and_remove_lower_bound(X,Slices) ->
+    smsarlb(X,Slices).
+
+smsarlb(X,[{{Lb,Ub},Part}|_]) when X >= Lb andalso X =< Ub -> 
+    {X-Lb,Part};
+smsarlb(X,[_|T]) ->
+    smsarlb(X,T).
     
