babff161b9e9fe2f899050acf6ea4e207c1bc330
Gianfranco Alongi <egiaalo@esekiws5098.rnd.ki.sw.ericsson.se>
2013-08-14 08:09:29 +0200

    Added second tests for when one number exists in the list

--- PhoneNumbers/Perl/phone_tests.pl
+++ PhoneNumbers/Perl/phone_tests.pl
@@ -1,7 +1,10 @@
-use Test::Simple tests => 1;
+use Test::Simple tests => 2;
 use strict;
 use warnings;
 use phone;
 
 my @numbers = ();
 ok( phone::check_numbers(\@numbers) eq "ok", "No numbers");
+
+@numbers = ("911");
+ok( phone::check_numbers(\@numbers) eq "ok", "One number is okay");
