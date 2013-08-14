6743cfd104c54f1785bd4daf24fce3854abb7e74
Gianfranco Alongi <egiaalo@esekiws5098.rnd.ki.sw.ericsson.se>
2013-08-14 12:26:34 +0200

    Refactoring - specific matching subroutine

--- PhoneNumbers/Perl/phone.pm
+++ PhoneNumbers/Perl/phone.pm
@@ -8,13 +8,20 @@ sub check_numbers
     if (scalar(@{$l}) == 2) {
 	my $first = ${$l}[0];
 	my $second = ${$l}[1];
-	if($second =~ /^$first.*/) {
-	    return "err";
-	}elsif($first =~ /^$second.*/){
+	if( any_prefixes_the_other($first, $second) ) {
 	    return "err";
 	}
     }
     return "ok"
+}
+
+sub any_prefixes_the_other
+{
+    if($_[1] =~ /^$_[0].*/) {
+	return "err";
+    }elsif($_[0] =~ /^$_[1].*/){
+	return "err";
+    }
 }
 
 1;
