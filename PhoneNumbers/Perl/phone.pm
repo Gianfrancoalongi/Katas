494de27d60698748163ea096cdd5ac7ea7f7556b
Gianfranco Alongi <egiaalo@esekiws5098.rnd.ki.sw.ericsson.se>
2013-08-14 12:18:45 +0200

    Added testcase where two numbers exist and they do not collide

--- PhoneNumbers/Perl/phone.pm
+++ PhoneNumbers/Perl/phone.pm
@@ -5,9 +5,15 @@ use warnings;
 sub check_numbers
 {
     my $l = $_[0];
-    if (scalar(@{$l}) > 1) {
-	return "err";
-    }else{	
+    if (scalar(@{$l}) == 2) {
+	my $first = ${$l}[0];
+	my $second = ${$l}[1];
+	if($second =~ /^$first.*/) {
+	    return "err";
+	}else{
+	    return "ok";
+	}
+    }else{
 	return "ok"
     }
 }
--- PhoneNumbers/Perl/phone_tests.pl
+++ PhoneNumbers/Perl/phone_tests.pl
@@ -1,4 +1,4 @@
-use Test::Simple tests => 3;
+use Test::Simple tests => 4;
 use strict;
 use warnings;
 use phone;
@@ -11,3 +11,6 @@ ok( phone::check_numbers(\@numbers) eq "ok", "One number is okay");
 
 @numbers = ("911", "9112");
 ok( phone::check_numbers(\@numbers) eq "err", "Two numbers that collide");
+
+@numbers = ("911", "822");
+ok( phone::check_numbers(\@numbers) eq "ok", "Two numbers that do not collide");
