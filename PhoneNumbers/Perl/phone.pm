8d4571f32b055d5514e1040b7f5d50faa77e6fd3
Gianfranco Alongi <egiaalo@esekiws5098.rnd.ki.sw.ericsson.se>
2013-08-16 07:41:10 +0200

    Refactoring. Simplifying the syntax.

--- PhoneNumbers/Perl/phone.pm
+++ PhoneNumbers/Perl/phone.pm
@@ -13,9 +13,9 @@ sub check_numbers
 sub check_every_number_against_all_others
 {
     my $l = $_[0];
-    for(my $i = 0; $i < scalar(@{$l}); $i++) {
+    for(my $i = 0; $i < scalar(@$l); $i++) {
 	my $first = ${$l}[$i];
-	for (my $j = $i+1; $j < scalar(@{$l}); $j++) {
+	for (my $j = $i+1; $j < scalar(@$l); $j++) {
 	    my $second = ${$l}[$j];
 	    if ( any_prefixes_the_other($first, $second) ) {
 		return "err";
