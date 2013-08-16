e60a88ef14a9ac3fa19328dc294b2ac3c41c0cf3
Gianfranco Alongi <egiaalo@esekiws5098.rnd.ki.sw.ericsson.se>
2013-08-16 07:29:23 +0200

    Refactoring. Extract method.

--- PhoneNumbers/Perl/phone.pm
+++ PhoneNumbers/Perl/phone.pm
@@ -6,17 +6,24 @@ sub check_numbers
 {
     my $l = $_[0];
     if (scalar(@{$l}) > 1) {
-	for(my $i = 0; $i < scalar(@{$l}); $i++) {
-	    my $first = ${$l}[$i];
-	    for (my $j = $i+1; $j < scalar(@{$l}); $j++) {
-		my $second = ${$l}[$j];
-		if ( any_prefixes_the_other($first, $second) ) {
-		    return "err";
-		}
-	    }	    
-	}
+	return check_every_number_against_all_others($l);
     }
     return "ok"
+}
+
+sub check_every_number_against_all_others
+{
+    my $l = $_[0];
+    for(my $i = 0; $i < scalar(@{$l}); $i++) {
+	my $first = ${$l}[$i];
+	for (my $j = $i+1; $j < scalar(@{$l}); $j++) {
+	    my $second = ${$l}[$j];
+	    if ( any_prefixes_the_other($first, $second) ) {
+		return "err";
+	    }
+	}
+    }
+    return "ok";
 }
 
 sub any_prefixes_the_other
